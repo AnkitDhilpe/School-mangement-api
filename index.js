5517c2a79045ecc8fc438be9e814a94db9882fa6
Ankit Dhilpe <121126372+AnkitDhilpe@users.noreply.github.com>
2025-03-29 17:43:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 
 // Create School Table if not exists
